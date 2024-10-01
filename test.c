914591f5cd5fe2ef7c7132c8182b6e81cbe31e5f
Fosssil <dahiyabadshah.fossil@gmail.com>
2024-10-01 15:08:35 +0530

    01-Oct-2024 03:08 PM

--- test.c
+++ test.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main() {
   int a = -5;
   int b = abs(a);
-  int c = time();
+  int c = rand();
   printf("Random number is: %d\n", c);
   printf("%d\n", b);
   return 0;
